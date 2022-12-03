1b4fe65d0954a61cebca985e6f896f63129de1ed
lursmanashvililuka <lukacho.lursmanashvili@gmail.com>
2022-12-03 14:49:24 +0400

    add files

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Card = ({ name, email, id }) => {
+    return(
+
+        <div className='tc bg-light-green dib br3 pas ma2 grow bu2 shadow-5'>
+            <img alt='robots' src={`https://robohash.org/${id} ?200x200x`} />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+
+        </div>
+    );
+
+}
+export default Card;
